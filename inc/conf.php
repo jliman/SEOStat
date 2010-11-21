2ad36114e8d14e89b7964bbcd644a2cef6f5a21e
jliman <jliman@proyekweb.com>
2010-11-21 23:55:46 +0700

    Add sender and recipient email address for notification

--- inc/conf.php
+++ inc/conf.php
@@ -1,19 +1,12 @@
 <?
-/*
+$notification_sender = 'jliman@gmail.com';
+$notification_recipient = 'jliman@gmail.com';
+
 $dbhost = 'localhost';
-$dbname = 'seostat';
+$dbname = 'seostat_20101121';
 $dbuser = 'seostat';
 $dbpasswd = 'seostat';
 $cookie = '/Users/jliman/Desktop/cookie.txt';
-*/
-
-$notification = 'jliman@gmail.com';
-
-$dbhost = 'localhost';
-$dbname = 'h34468_seostat';
-$dbuser = 'h34468_seostat';
-$dbpasswd = 'qwerty123';
-$cookie = '/www/seostat/cookie.txt';
 
 error_reporting(0);
 ?>
--- inc/notification.php
+++ inc/notification.php
@@ -1,14 +1,15 @@
 <?php
 function shutdown () {
-	global $notification;
+	global $notification_recipient, $notification_sender;
 	$message = ob_get_clean();
 	// To send HTML mail, the Content-type header must be set
 	$headers  = 'MIME-Version: 1.0' . "\r\n";
 	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 	// Additional headers
-	$headers .= 'From: SEOStat auroraku.com <sales@auroraku.com>' . "\r\n";
+	$headers .= 'From: SEOStat <'.$notification_sender.'>' . "\r\n";
 	$headers .= 'X-Mailer: PHP/' . phpversion();
-	mail($notification, 'SEOStat Fetch Result '.date("Y-m-d H:i:s"), $message, $headers);
+	mail($notification_recipient, 'SEOStat Fetch Result '.date("Y-m-d H:i:s"), $message, $headers);
+	//echo $message;
 }
 register_shutdown_function('shutdown');
 ?>
