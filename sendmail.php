e38695d019a430daac6eafa46db16828f05299ab
jliman <jliman@proyekweb.com>
2010-11-22 01:11:38 +0700

    Remove

--- sendmail.php
+++
@@ -1,7 +0,0 @@
-<?
-$headers = "From: julius@liman.org\r\n";
-$headers .= "Reply-to: julius@liman.org\r\n";
-$mail_sent = @mail("jliman@gmail.com", "Judul TEST", "Isi TEST", $headers);
-echo $mail_sent ? "Terkirim" : "
-Gagal";
-?> 
